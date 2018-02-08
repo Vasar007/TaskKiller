7f4c4e439e98d603c1e2db4c4847037051703771
Vasar007 <vasar007@yandex.ru>
2018-02-08 22:51:43 +0300

    Upload project

---
+++ Source/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+
+namespace TaskKiller
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
